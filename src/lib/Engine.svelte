b842ac83cecf286a709cce4c6a141cb0adf5dd04
Emre Özcan <67864140+eoezcan2@users.noreply.github.com>
2024-07-26 14:37:14 +0200

    Add comments

--- src/lib/Engine.svelte
+++ src/lib/Engine.svelte
@@ -5,19 +5,19 @@
     const delay : number = 50;  // Base delay of each piston in ms
     const speed : number = 100; // Delay between each stroke in ms
 
-    export let type : EngineType;
-    export let speedMultiplier : number;
+    export let type : EngineType;   // Type of the engine
+    export let speedMultiplier : number;    // Speed multiplier of the engine
 
-    const pistons : number[] = [1, 2, 3, 4, 5, 6];
+    const pistons : number[] = [1, 2, 3, 4, 5, 6];  // Piston count
 
     /**
      * Notifies each piston component to run
      */
     function startEngine() {
-        pistons.forEach((_, index) => {
-            engineStore.update(store => {
-                store[index] = true;
-                return store;
+        pistons.forEach((_, index) => { // For each piston
+            engineStore.update(store => {   // Update the engine store
+                store[index] = true;    // Set the piston to active
+                return store;   // Return the updated store
             });
         });
     }
@@ -26,29 +26,35 @@
      * Notifies each piston component to stop
      */
     function stopEngine() {
-        pistons.forEach((_, index) => {
-            engineStore.update(store => {
-                store[index] = false;
-                return store;
+        pistons.forEach((_, index) => { // For each piston
+            engineStore.update(store => {   // Update the engine store
+                store[index] = false;   // Set the piston to idle
+                return store;   // Return the updated store
             });
         });
     }
 
 </script>
 
-<button on:click={startEngine}>Start Engine</button>
-<button on:click={stopEngine}>Stop Engine</button>
+<input type="button" on:click={startEngine} value="Start Engine">
+<input type="button" on:click={stopEngine} value="Stop Engine">
 
-{#if type === "Inline 4"}
-    <Piston index="0" delay={delay} speed={speed * speedMultiplier} />
-    <Piston index="1" delay={delay * 2} speed={speed * speedMultiplier} />
-    <Piston index="2" delay={delay * 2} speed={speed * speedMultiplier} />
-    <Piston index="3" delay={delay} speed={speed * speedMultiplier} />
-{:else if type === "Inline 2"}
-    <Piston index="0" delay={delay} speed={speed * speedMultiplier} />
-    <Piston index="1" delay={delay * 2} speed={speed * speedMultiplier} />
-{:else if type === "Single"}
-    <Piston index="0" delay={delay} speed={speed * speedMultiplier} />
-{:else }
-    Not implemented yet!
-{/if}
+<div id="pistons">
+    {#if type === "Inline 4"}
+        <Piston index="0" delay={delay} speed={speed * speedMultiplier} />
+        <Piston index="1" delay={delay * 2} speed={speed * speedMultiplier} />
+        <Piston index="2" delay={delay * 2} speed={speed * speedMultiplier} />
+        <Piston index="3" delay={delay} speed={speed * speedMultiplier} />
+    {:else if type === "Inline 2"}
+        <Piston index="0" delay={delay} speed={speed * speedMultiplier} />
+        <Piston index="1" delay={delay * 2} speed={speed * speedMultiplier} />
+    {:else if type === "Single"}
+        <Piston index="0" delay={delay} speed={speed * speedMultiplier} />
+    {:else }
+        Not implemented yet!
+    {/if}
+</div>
+
+<style>
+
+</style>
--- src/lib/Piston.svelte
+++ src/lib/Piston.svelte
@@ -2,50 +2,60 @@
     import { onMount } from 'svelte';
     import { engineStore } from './engineStore.ts';
 
-    const pathIdle : string = "/piston-idle.svg";
-    const pathActive : string = "/piston-active.svg";
+    const pathIdle : string = "/piston-idle.svg";   // Path of the piston when idle
+    const pathActive : string = "/piston-active.svg";   // Path of the piston when active
 
-    export let index : number;
-    export let delay : number;
-    export let speed : number;
+    export let index : number;  // Piston index
+    export let delay : number;  // Delay of each piston in ms
+    export let speed : number;  // Delay between each stroke in ms
 
-    let isRunning : boolean = false;
-    let pathCurrent : string = pathIdle;
+    let isRunning : boolean = false;    // Is the piston running?
+    let pathCurrent : string = pathIdle;    // Current path of the piston
 
     onMount(() => {
-        engineStore.subscribe(store => {
-            if (store[index]) {
-                run();
-            } else {
-                stop();
+        engineStore.subscribe(store => {    // Subscribe to the engine store
+            if (store[index]) { // If the piston is active
+                run();  // Run the piston
+            } else {    // If the piston is idle
+                stop(); // Stop the piston
             }
         });
     });
 
+    /**
+     * Moves the piston up
+     */
     function up() {
         pathCurrent = pathActive;
     }
 
+    /**
+     * Moves the piston down
+     */
     function down() {
         pathCurrent = pathIdle;
     }
 
+    /**
+     * Runs the piston up and down
+     */
     async function run() {
-        if (isRunning) return;
-        isRunning = true;
-        await new Promise(resolve => setTimeout(resolve, delay));
-        while (isRunning) {
-            // sleep
-            await new Promise(resolve => setTimeout(resolve, speed));
-            up();
-            await new Promise(resolve => setTimeout(resolve, speed));
-            down();
-            console.log(speed);
+        if (isRunning) return;  // If the piston is already running, return
+        isRunning = true;   // Set the piston to running
+        await new Promise(resolve => setTimeout(resolve, delay));   // Delay before starting to create a more realistic effect
+        while (isRunning) { // While the piston is running
+            await new Promise(resolve => setTimeout(resolve, speed));   // Delay between each stroke
+            up();   // Move the piston up
+            await new Promise(resolve => setTimeout(resolve, speed));   // Delay between each stroke
+            down(); // Move the piston down
         }
     }
 
+    /**
+     * Stops the piston
+     */
     function stop() {
-        isRunning = false;
+        isRunning = false;  // Set the piston to not running
     }
 </script>
 
